083cbd7badae506bcf6786c846989d9c2ccc58b7
Patrick <calimerorulez@gmail.com>
2023-05-24 11:46:56 +0200

    Removed Doplarr

--- doplarr/build/build.clj
+++
@@ -1,8 +0,0 @@
-(ns build
-  (:require
-   [org.corfield.build :as bb]))
-
-(defn uber [_]
-  (bb/clean nil)
-  (bb/uber {:uber-file "target/doplarr.jar"
-            :main 'doplarr.core}))
--- doplarr/src/doplarr/state.clj
+++
@@ -1,9 +0,0 @@
-(ns doplarr.state
-  (:require
-   [clojure.core.cache.wrapped :as cache]))
-
-(def cache (cache/ttl-cache-factory {} :ttl 900000))
-
-(def discord (atom nil))
-
-(def config (atom nil))
